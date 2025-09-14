223293a56d017c057d97f5f588d750806dbaba21
Offizil <fredoffizil@gmail.com>
2025-09-14 13:15:28 +0100

    fixed errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const mongodb = require('../database/connection');
-const objectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 
 // mongodb.getDatabase.db()
@@ -18,8 +18,11 @@ const getAllUsers = async (req, res) => {
 
 
 const getSingleUser = async (req, res) => {
-    const userId = new  (req.params.id);
-    const result = await mongodb.getDatabase.db().collection('users').find()({ _id: userId });
+    const userId = new ObjectId (req.params.id);
+
+    const db = mongodb.getDatabase().db();
+    const result = await db.collection('users').find({ _id: userId });
+
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
